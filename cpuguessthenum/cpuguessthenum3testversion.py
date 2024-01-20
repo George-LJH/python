5d6914948f6e1f2b13638941499c0e4e94b9cb86
George <155213581+George-LJH@users.noreply.github.com>
2024-01-20 04:59:05 +0000

    fix bug

--- cpuguessthenum/cpuguessthenum3testversion.py
+++ cpuguessthenum/cpuguessthenum3testversion.py
@@ -9,7 +9,13 @@ def askinteger():
             return val 
         except ValueError:
             print("That's not an integer! Try again.")
-        except if val not 
+def askintegerbetween(low,high):
+    temp=askinteger()
+    while (temp<low) or (temp>high):
+        print("please input a number between 0 to 100.")
+        temp=askinteger()
+    return temp
+    
 correct_answer=random.randint(0,100)
 min=0
 max=100
@@ -17,47 +23,36 @@ cpuguess=(math.ceil((min+max)/2))
 numofguess=8
 score=0
 print("Think of a number between 0 to 100 and the cpu will guess it!")
-answer=askinteger()
+answer=askintegerbetween(0,100)
 while True:
+    if cpuguess==answer:
         max=100
         min=0
         numofguess=8
         score=score+1
         print("Your number is...",answer,"!")
         print("Think of a number between 0 to 100 and the cpu will guess it!")
-        answer=askinteger()
-        elif numofguess==0:
-            max=100
-            min=0
-            numofguess=8
-            score=score-1
-            print("Game over! The cpu can't guess your number! What is your new number?")
-            answer=askinteger()
-        if cpuguess>answer:
-            if cpuguess<max:
-                print("1...")
-                time.sleep(0.5)
-                print("2...")
-                time.sleep(0.5)
-                print("3...")
-                time.sleep(0.5)
-                print("Cpu is guessing...",cpuguess)
-                print("Incorrect!")
-                max=cpuguess
-                print(min,"to",max)
-                cpuguess=(math.ceil((min+max)/2))
-                time.sleep(1)
-        if cpuguess<answer:  #guess=50  correct answer=70
-            if cpuguess>min:
-                print("1...")
-                time.sleep(0.5)
-                print("2...")
-                time.sleep(0.5)
-                print("3...")
-                time.sleep(0.5)
-                print("Cpu is guessing...",cpuguess)
-                print("Incorrect!")
-                min=cpuguess
-                print(min,"to",max)
-                cpuguess=(math.ceil((min+max)/2))
-                time.sleep(1)
+        answer=askintegerbetween(0,100)
+    elif numofguess==0:
+        max=100
+        min=0
+        numofguess=8
+        score=score-1
+        print("Game over! The cpu can't guess your number! What is your new number?")
+        answer=askintegerbetween(0,100)
+    if cpuguess>answer:
+        if cpuguess<max:
+            print("Cpu is guessing...",cpuguess)
+            print("Incorrect!")
+            max=cpuguess
+            print(min,"to",max)
+            cpuguess=(math.ceil((min+max)/2))
+            time.sleep(1)
+    if cpuguess<answer:  #guess=50  correct answer=70
+        if cpuguess>min:
+            print("Cpu is guessing...",cpuguess)
+            print("Incorrect!")
+            min=cpuguess
+            print(min,"to",max)
+            cpuguess=(math.ceil((min+max)/2))
+            time.sleep(1)
