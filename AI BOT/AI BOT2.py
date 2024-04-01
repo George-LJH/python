b05a09e5891d71853bd5536a7bacdec05c08c904
George <155213581+George-LJH@users.noreply.github.com>
2024-04-01 14:42:09 +0000

    aa

--- AI BOT/AI BOT2.py
+++ AI BOT/AI BOT2.py
@@ -1,28 +1,35 @@
 print("AI: Hello. How may I help you today? You can say simple commands like:")
 
-
+print(" Hi")
 print(" Tell me a knock knock joke")
 print(" Knock Knock")
 print(" Tell me a joke")
+print(" exit()")
 user= input()
 # user=user.lower()
 
 while True:
 
     if user.lower()=="knock knock":
-        print("who's there?")
+        print("AI: who's there?")
         user=input()
         print(user,"who?")
-        print("Ha ha! That's a funny joke!")
+        user=input()
+        print("AI: Ha ha! That's a funny joke!")
         user=input()
         # user=user.lower()
     elif user.lower()=="tell me a joke":
-        print("Where do pigs go to school?")
-        print("Hogwarts!")
+        print("AI: Where do pigs go to school?")
+        print("AI: Hogwarts!")
         user=input()
+    elif user.lower()=="exit()":
+        print("AI: Bye!")
+        exit()
+    elif user.lower()=="hi":
+        print("AI: Hello! Nice to meet you!")
         # user=user.lower()
-    elif user.lower()!= "knock knock" or "tell me a joke":
-        print("I don't understand you... Can you repeat your request?")
+    elif user.lower()!= "knock knock" or "tell me a joke" or "exit()" or "hi":
+        print("AI: I don't understand you... Can you repeat your request?")
         user=input()
 
 
